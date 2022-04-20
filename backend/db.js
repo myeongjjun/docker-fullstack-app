469bc94dd3d3bbfc19c066bdb1daab8eb02b670b
myeongjjun <mjk3026@gmail.com>
2022-04-21 02:06:51 +0900

    db.js fixed

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: "mysql",
+    host: "docker-fullstack-mydb.chivm7cb6gwi.ap-northeast-2.rds.amazonaws.com",
     user: "root",
     password: "myeong77",
     database: "myapp",
